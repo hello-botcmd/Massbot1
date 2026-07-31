29f28a7eaeac0978366cdb43132b7f76bb0f9d2c
hello-botcmd <swaruppanda2008@gmail.com>
2026-07-31 13:01:31 +0530

    Add St class with various constants for bot states

---
+++ bot/states.py
@@ -0,0 +1,11 @@
+class St:
+    # add account
+    ADD_METHOD, SINGLE_SESSION, BULK_COUNT, BULK_SESSION = range(4)
+    # join
+    JOIN_LINK, JOIN_COUNT, JOIN_MIN, JOIN_MAX = range(4, 8)
+    # mode
+    MODE_COUNTS = 8
+    # reaction
+    REACT_LINK, REACT_COUNT, REACT_EMOJI = range(9, 12)
+    # views
+    VIEW_LINK, VIEW_COUNT = range(12, 14)
